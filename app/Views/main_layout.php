9a339b3f6b5f9d5ef89e5d8d847f4b42393196e9
Gustavo Vieira Coelho <gv0669972@gmail.com>
2024-08-26 21:23:39 -0300

    Subindo o repositório já em andamento

---
+++ app/Views/main_layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=base_url('recursos/bootstrap/bootstrap.main.css')?>">
+    <link rel="stylesheet" href="<?=base_url('recursos/css/estilo.css')?>">
+    <title>ARA Cursos</title>
+</head>
+<body>
+
+    <?=$this->include('nav')?>
+
+    <script>src="<?=base_url('recursos/bootstrap/bootstrap.bundle.min.js')?>"</script>
+</body>
+</html>
---
+++ app/Views/nav.php
@@ -0,0 +1,14 @@
+<nav class="container-fluid">
+    <div class="row aling-itens-center">
+        <div class="col p-3">
+            <a href="">
+                <img src="<?=base_url('recursos/imagens/logo.png')?>" alt="logo descricao">
+            </a>
+        </div>
+        <div>
+            <div> <a href=""> Início </a> </div>
+            <div> <a href=""> Cursos </a> </div>
+            <div> <a href=""> Onde estamos? </a> </div>
+        </div>
+    </div>
+</nav>
